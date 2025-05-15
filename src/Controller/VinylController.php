473fd5367882fcf1926e63258cab0cb03be70679
psaboureau <psaboureau@gmx.fr>
2025-05-15 15:00:19 +0200

    05. Symfony Flex: Aliases, Packs & Recipes

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
+
+class VinylController  
+{
+    #[Route('/')]
+    public function homepage() : Response
+    {
+       return new Response('Title: PB & Jams'); 
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null) : Response
+    {
+
+        if ($slug) 
+        {
+            $title = u(str_replace('-', ' ', $slug))->title(true);
+        } else 
+        {
+            $title = 'All Genres';
+        }
+
+
+        return new Response('Genre: '.$title);
+
+    }
+}
