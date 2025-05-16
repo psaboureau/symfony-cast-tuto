dc9bf656ec2d693a214d358109ad1efdf03794ca
psaboureau <psaboureau@gmx.fr>
2025-05-16 14:23:28 +0200

    14. Service Objects

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -9,8 +9,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SongController extends AbstractController 
 {
-    #[Route('/api/songs/{id}')]
-    public function getSong($id) : Response 
+    #[Route('/api/songs/{id}', methods: ['GET'])]
+    public function getSong(int $id) : Response 
     {
        // TODO query the database
        $song = [
